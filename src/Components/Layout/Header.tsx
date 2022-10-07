e33f3112d1556b13e82c7780b91fca51c3d8ce58
A <movie3342@gmail.com>
2022-10-07 15:47:31 +0900

    feat:MainLayout 컴포넌트 구현
    MainLayout 컴포넌트를 구현하여 기본 레이아웃을 적용하였습니다.
    Header
    - header 태그 적용
    Main
    - main 태그 안에 Outlet을 가지고 있어 Route에서 컴포넌트가 랜더링 될때 이 부분에 랜더링됩니다.

---
+++ src/Components/Layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.header`
+  height: 6rem;
+  color: #fff;
+  background-color: black;
+`;
+
+const Header = () => {
+  return <Container>Header</Container>;
+};
+
+export default Header;
---
+++ src/Components/Layout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import Main from "./Main";
+
+const Container = styled.div``;
+
+const MainLayout = () => {
+  return (
+    <Container>
+      <Header />
+      <Main />
+    </Container>
+  );
+};
+
+export default MainLayout;
