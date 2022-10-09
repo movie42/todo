9c0bb634fdfb997f8732aeaf3d73ef1504c0d029
movie42 <movie3342@gmail.com>
2022-10-09 21:52:58 +0900

    localhost, validate hook 위치 변경

--- src/lib/hook/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as useValidate } from "./useValidate";
---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,2 @@
+export { default as useValidate } from "./useValidate";
+export { default as useLocalStorage } from "./useLocalStorage";
